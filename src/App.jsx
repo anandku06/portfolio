725137222152385ac4fcfd0cda8fcc6fc9e54f30
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-04-24 00:25:13 +0530

    Initialize React project with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold underline bg-gray-200'>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
