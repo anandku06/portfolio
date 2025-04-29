90d76c406fa8ab3f731351e1daa9b1c66073fa3d
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-04-30 03:55:15 +0530

    feat: add 3D astronaut model and integrate into Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import HeroText from '../components/HeroText'
 import ParallaxBackground from '../components/ParallaxBackground'
+import { Canvas } from '@react-three/fiber'
+import { Astronaut } from '../components/Astronaut'
+import { OrbitControls } from '@react-three/drei'
 
 const Hero = () => {
   return (
     <section className='flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space'>
         <HeroText />
         <ParallaxBackground />
+        <figure className='absolute inset-0' style={{width : "100vw", height : "100vh"}}>
+          <Canvas>
+            <Astronaut />
+            <OrbitControls />
+          </Canvas>
+        </figure>
     </section>
   )
 }
