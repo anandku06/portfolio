340be0b881c98cb00566cde796d729945daa0492
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-05-04 23:51:33 +0530

    feat: enhance About section with Card components for improved visual representation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { motion } from "motion/react";
+import React from "react";
 
-const Card = () => {
-  return (
-    <div>Card</div>
-  )
-}
+const Card = ({ style, text, image, containerRef }) => {
+  return image && !text ? (
+    <motion.img
+      src={image}
+      className="absolute w-15 cursor-grab"
+      style={style}
+      whileHover={{ scale: 1 }}
+      drag
+      dragConstraints={containerRef}
+    />
+  ) : (
+    <motion.div
+      className="absolute px-1 py-4 text-xl text-center rounded-full ring ring-gray-700 font-extralight bg-storm w-[12rem] cursor-grab"
+      style={style}
+      whileHover={{ scale: 1 }}
+      drag
+      dragConstraints={containerRef}
+    >
+      {text}
+    </motion.div>
+  );
+};
 
-export default Card
+export default Card;
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
+import Card from "../components/Card";
 
 const About = () => {
+  const grid2Container = useRef();
   return (
     <section className="c-space section-spacing">
       <h2 className="text-heading">About Me</h2>
@@ -23,11 +25,52 @@ const About = () => {
         </div>
 
         <div className="grid-default-color grid-2">
-            <div className="flex items-center justify-center w-full h-full">
-                <p className="flex items-end text-5xl text-gray-500">
-                    PHILOSOPHE
-                </p>
-            </div>
+          <div
+            ref={grid2Container}
+            className="flex items-center justify-center w-full h-full"
+          >
+            <p className="flex items-end text-5xl text-gray-500">PHILOSOPHE</p>
+            <Card
+              style={{ rotate: "75deg", top: "30%", left: "20%" }}
+              text="GRASP"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-30deg", top: "60%", left: "45%" }}
+              text="SOLID"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "90deg", bottom: "30%", left: "70%" }}
+              text="Design"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "55%", left: "0%" }}
+              text="Design Principles"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "20deg", top: "10%", left: "30%" }}
+              text="SRP"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "30deg", top: "70%", left: "70%" }}
+              image="assets/logos/csharp-pink.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "70%", left: "25%" }}
+              image="assets/logos/dotnet-pink.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "5%", left: "10%" }}
+              image="assets/logos/blazor-pink.png"
+              containerRef={grid2Container}
+            />
+          </div>
         </div>
         <div className="grid-black-color grid-3"></div>
         <div className="grid-special-color grid-4"></div>
